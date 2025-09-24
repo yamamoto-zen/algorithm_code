466ac9ce826baed589c814b0d2b361827b9b9ec9
yamamoto-zen <yamamoto.keitaro@zen-integration.com>
2025-09-25 05:06:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ from framework import Alg3D, Board
 class MyAI(Alg3D):
     def get_move(self, board: Board) -> Tuple[int, int]:
         #ここから自由にアルゴリズムを記入
+        legal_moves = []
+                for x in range(4):
+                    for y in range(4):
+                        # zが埋まっていなければ合法手
+                        for z in range(4):
+                            if board[z][y][x] == 0:
+                                legal_moves.append((x, y))
+                                break
+                # ランダムに選択
+                return random.choice(legal_moves)
