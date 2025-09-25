871784a51cf7eb44d6ad9ae8e5f7a80856c10528
yamamoto.keitaro <yamamoto.keitaro@zen-integration.com>
2025-09-25 11:35:11 +0900

    あい

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from typing import List, Tuple
 import random  # ← これが必須
-from local_driver import Alg3D, Board   # ローカル検証用
-# from framework import Alg3D, Board
+# from local_driver import Alg3D, Board   # ローカル検証用
+from framework import Alg3D, Board
 
 class MyAI(Alg3D):
     def get_move(
